256dbb916bfd654b312b69d985dcb55b809c14aa
DanyStark10 <90022009+DanyStark10@users.noreply.github.com>
2022-11-03 22:54:18 -0700

    update-css-html

--- src/controllers/BlogController.php
+++ src/controllers/BlogController.php
@@ -3,11 +3,15 @@
 namespace App\Controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
 
 class BlogController{
     function index (Request $request, Response $response, $args) {
-        $response->getBody()->write("Ultimos blogs publicados desde la clase");
-        return $response;
+        $view = Twig::fromRequest($request);
+        $parametros = [
+            'title' => "Mi Blogs"
+        ];
+        return $view->render($response, "blog.html",$parametros);
     }
 
 }
--- src/controllers/ContactoController.php
+++ src/controllers/ContactoController.php
@@ -3,11 +3,14 @@
 namespace App\Controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
 
 class ContactoController{
-    function index(Request $request, Response $response, $args) {
-        $response->getBody()->write("Contactame!");
-        return $response;
+    function index (Request $request, Response $response, $args) {
+        $view = Twig::fromRequest($request);
+        $parametros = [
+            'title' => "Mis contactos"
+        ];
+        return $view->render($response, "contacto.html",$parametros);
     }
-
 }
